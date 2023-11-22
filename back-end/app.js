43a9749e43e25824d2e87e762eead6d11676e687
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2023-11-22 13:35:35 +0200

    init socket

--- back-end/app.js
+++ back-end/app.js
@@ -1,13 +1,49 @@
 const express = require('express');
-const app = express();
-const port = 3000; // choose a port number
+const http = require('http');
+const socketIO = require('socket.io');
+const cors = require('cors'); // Import the cors middleware
 
-// Define a route
-app.get('/', (req, res) => {
-    res.send('Hello, World!');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server, {
+    cors: {
+        methods: ['GET', 'POST'],
+    },
 });
 
-// Start the server
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+
+app.use(cors());
+
+let mentorSocket = null;
+
+io.on('connection', (socket) => {
+    console.log('User connected');
+
+    socket.on('joinRoom', (roomId) => {
+        socket.join(roomId);
+        console.log(`User joined room: ${roomId}`);
+
+        if (!mentorSocket) {
+            mentorSocket = socket;
+            // Send a message to the mentor indicating their mentor status
+            mentorSocket.emit('mentorStatus', true);
+        }
+    });
+
+    socket.on('codeChange', ({ roomId, code }) => {
+        io.to(roomId).emit('codeChange', code);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+        if (mentorSocket) {
+            mentorSocket = null;
+        }
+    });
+});
+
+const port = 8080; // Choose a port number for the WebSocket server
+server.listen(port, () => {
+    console.log(`WebSocket server is running on http://localhost:${port}`);
 });
--- front-end/src/pages/Code.js
+++ front-end/src/pages/Code.js
@@ -1,17 +1,67 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
+import io from 'socket.io-client';
+import '../Styles/Code.css';
 
 const Code = () => {
     const { id } = useParams();
+    const [code, setCode] = useState('');
+    const [socket, setSocket] = useState(null);
+    const [isMentor, setIsMentor] = useState(false);
+
+
+    useEffect(() => {
+        console.log('useEffect');
+        const newSocket = io('ws://localhost:8080');
+        setSocket(newSocket);
+
+        return () => {
+            newSocket.disconnect();
+        };
+    }, []);
+
+    useEffect(() => {
+        if (socket) {
+            socket.emit('joinRoom', id);
+
+            socket.on('codeChange', (newCode) => {
+                setCode(newCode);
+            });
+            socket.on('mentorStatus', (status) => {
+                setIsMentor(status);
+            });
+        }
+    }, [id, socket]);
+
+    const handleCodeChange = (event) => {
+        const newCode = event.target.value;
+        setCode(newCode);
+
+        socket.emit('codeChange', { roomId: id, code: newCode });
+    };
+    useEffect(() => { }, [isMentor])
 
     return (
-        <div>
+        <div className="code-div">
+            {isMentor &&
+                <h1>Mentor</h1>
+            }
             <h1>Code block details</h1>
-            <p>Name: {id}</p>
-            {/* Add more details about the code block here */}
+            <p> {id}</p>
+
+            <div className="code-block-container">
+                <textarea
+                    className="code-block"
+                    placeholder="Write your code here..."
+                    value={code}
+                    onChange={handleCodeChange}
+                    rows={20}
+                    cols={50}
+                    readOnly={isMentor}
+                />
+            </div>
         </div>
-    )
+    );
 }
 
-export default Code
+export default Code;
