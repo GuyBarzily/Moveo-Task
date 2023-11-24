6f057494815da2e303c43962625039c770737fe5
Guy Barzily <85988766+GuyBarzily@users.noreply.github.com>
2023-11-24 11:21:50 +0200

    added highlight for the code

--- front-end/src/pages/Code.js
+++ front-end/src/pages/Code.js
@@ -5,25 +5,24 @@ import '../Styles/Code.css';
 import { getCodeByTitle } from '../axios';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faFaceSmile, faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-import CodeHighlighter from '../components/CodeHighlighter';
-// import AceEditor from "react-ace";
 
-// import "ace-builds/src-noconflict/mode-javascript";
-// import "ace-builds/src-noconflict/theme-github";
-// import "ace-builds/src-noconflict/ext-language_tools";
+import Editor from 'react-simple-code-editor';
+import { highlight, languages } from 'prismjs';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/themes/prism.css'; //Example style, you can use another
+
 
 
 
 const Code = () => {
     const { id } = useParams();
-    const navigate = useNavigate();
     const [code, setCode] = useState('');
     const [socket, setSocket] = useState(null);
     const [isMentor, setIsMentor] = useState(false);
-    const textareaRef = useRef(null);
     const [codeData, setCodeData] = useState(null)
     const [success, setSuccess] = useState(false);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
 
 
     useEffect(() => {
@@ -37,9 +36,15 @@ const Code = () => {
 
     useEffect(() => {
         const fetchData = async () => {
-            const data = await getCodeByTitle(id);
-            setCodeData(data[0]);
-            setCode(data[0].code);
+            try {
+                const data = await getCodeByTitle(id);
+                setCodeData(data[0]);
+                setCode(data[0].code);
+            } catch (e) {
+                console.log(e);
+            } finally {
+                setLoading(false)
+            }
         }
         fetchData();
         if (socket) {
@@ -57,12 +62,7 @@ const Code = () => {
         }
     }, [id, socket, isMentor]);
 
-    const goBack = () => {
-        navigate(-1);
-    };
-
-    const handleCodeChange = (event) => {
-        const newCode = event.target.value;
+    const handleCodeChange = (newCode) => {
         setCode(newCode);
         checkSuccess(newCode);
         socket.emit('codeChange', { roomId: id, code: newCode });
@@ -82,34 +82,36 @@ const Code = () => {
 
     return (
         <div className="code-div">
-            {/* {isMentor &&
-                <h1>Mentor</h1>
-            } */}
             <h1>Code block details</h1>
             <h2> {id}</h2>
             {loading ? (
                 <div className="loader"></div>
             ) : (
                 <div >
-                    <textarea
-                        ref={textareaRef}
-                        className={`code-block ${isMentor ? 'read-only' : ''}`}
-                        placeholder="Write your code here..."
+                    <Editor
                         value={code}
-                        onChange={handleCodeChange}
-                        rows={20}
-                        cols={50}
+                        onValueChange={handleCodeChange}
+                        highlight={code => highlight(code, languages.js)}
+                        padding={10}
                         readOnly={isMentor}
-                        spellCheck={false}
+                        style={{
+                            backgroundColor: 'white',
+                            minHeight: '50vh',
+                            minWidth: '40vw',
+                            fontFamily: 'monospace',
+                            fontSize: 20,
+                            border: '1px solid #ddd',
+                            cursor: isMentor ? 'default' : 'text',
+
+
+                        }}
                     />
-                    <CodeHighlighter code={code} />
+
                 </div>
             )}
-
-
             {success &&
                 <>
-                    <FontAwesomeIcon icon={faFaceSmile} size='10x' color='orange' />
+                    <FontAwesomeIcon icon={faFaceSmile} size='10x' color='orange' style={{ marginTop: '20px' }} />
                     <h1>Very Good!</h1>
                 </>
             }
